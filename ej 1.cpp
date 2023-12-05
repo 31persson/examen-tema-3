a4f77184482b4966bec421689707e370a208d53c
31persson <jperscas@myuax.com>
2023-12-05 11:18:37 +0100

    ej 1, 2 y 3

--- ej 1.cpp
+++ ej 1.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
+#include <stdexcept>
 #include <map>
 #include <string>
 #include <variant>
-#include <stdexcept>
 
 class Variant {
 public:
@@ -62,22 +62,45 @@ public:
             throw std::runtime_error("Símbolo '" + name + "' no encontrado en el entorno.");
         }
     }
+
+    void remove(const std::string& name) {
+        auto it = symbolTable.find(name);
+        if (it != symbolTable.end()) {
+            symbolTable.erase(it);
+        } else {
+            throw std::runtime_error("No se puede eliminar. Símbolo '" + name + "' no encontrado en el entorno.");
+        }
+    }
+
+    bool exists(const std::string& name) const {
+        return symbolTable.find(name) != symbolTable.end();
+    }
 };
 
 int main() {
     try {
         Environment env;
 
-        env.insert("playerScore", Variant(100));
-
-        Variant score = env.lookup("playerScore");
+        // Almacenar y recuperar variables
+        env.setVariable("playerScore", Variant(100));
+        Variant score = env.getVariable("playerScore");
         std::cout << "Player Score: " << score.get<Variant::IntType>() << std::endl;
 
-        // Intentar buscar un símbolo que no existe
-        Variant nonExistent = env.lookup("nonExistentSymbol");
+        // Insertar variable con manejo de excepciones
+        env.insert("newVariable", Variant(42));
 
-        // Intentar insertar un símbolo que ya existe
-        env.insert("playerScore", Variant(200));
+        // Verificar si un símbolo existe
+        if (env.exists("playerScore")) {
+            std::cout << "El símbolo 'playerScore' existe en el entorno." << std::endl;
+        } else {
+            std::cout << "El símbolo 'playerScore' no existe en el entorno." << std::endl;
+        }
+
+        // Eliminar un símbolo
+        env.remove("playerScore");
+
+        // Intentar obtener un símbolo eliminado
+        Variant deletedSymbol = env.lookup("playerScore");
     } catch (const std::exception& e) {
         std::cerr << "Error: " << e.what() << std::endl;
     }
